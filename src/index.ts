ce161038da56334ef1f735534d469689665c5db1
obzenner <dmitry.lambrianov@schibsted.com>
2022-12-02 17:54:36 +0100

    day2: part1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-import { day1 } from './solutions/day1/index';
+import { day1 } from './solutions/day1';
+import { day2 } from './solutions/day2';
 
 
 (async () => {
-    const res = await day1();
+    const day1res = await day1();
+    const day2res = await day2();
+
+    const res = {
+        day1: day1res,
+        day2: day2res
+    }
     console.log(res);
 })();
