9b3e3f00030ae39829e678a5dbdf343515d83426
obzenner <dmitry.lambrianov@schibsted.com>
2022-12-05 23:06:42 +0100

    day5

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { day1 } from './solutions/day1';
 import { day2 } from './solutions/day2';
 import { day3 } from './solutions/day3';
 import { day4 } from './solutions/day4';
+import { day5 } from './solutions/day5';
 
 
 (async () => {
@@ -9,12 +10,14 @@ import { day4 } from './solutions/day4';
     const day2res = await day2();
     const day3res = await day3();
     const day4res = await day4();
+    const day5res = await day5();
 
     const res = {
         day1: day1res,
         day2: day2res,
         day3: day3res,
-        day4: day4res
+        day4: day4res,
+        day5: day5res
     }
     console.log(res);
 })();
---
+++ src/solutions/day5/index.ts
@@ -0,0 +1,102 @@
+import loadFile from "../../utils/loadFile";
+import { splitByEmptyLine } from "../../utils/splitByEmptyLine";
+
+type Program = Record<'q' | 'd' | 'o', number>[];
+
+const isEmpty = (crate: string) => crate === ' ';
+
+const exe = (input: Map<number, string[]>, program: Program, reverse = true) => {
+    const inputMap = new Map(input); // to avoid mutation
+
+    for (let i = 0; i < program.length; i++) {
+        const origin = program[i].o;
+        const destination = program[i].d;
+        const quantity = program[i].q;
+
+        if (origin === 0) {
+            console.log(program[i], i);
+        }
+
+        const originStack = inputMap.get(origin);
+        const destinationStack = inputMap.get(destination);
+
+        const toMoveSlice = originStack.slice(originStack.length - quantity);
+        const final = reverse ? toMoveSlice.reverse() : toMoveSlice; // too lazy to make this performant
+
+        const newDestination = [...destinationStack, ...final];
+        const newOrigin = [...originStack.slice(0, originStack.length - quantity)];
+        
+        inputMap.set(origin, newOrigin);
+        inputMap.set(destination, newDestination);
+    }
+
+    const res = [...inputMap.values()].reduce((acc, stack) =>
+        acc + stack[stack.length - 1],
+    '');
+
+    return res;
+}
+
+const getMappedCrates = (crates: string[], crateCharIndexes: number[]) => {
+    const mappedCrates = crates.reduce((acc, line, index) => {
+        if (index === crates.length - 1) { // ignore row with numbers
+            return acc;
+        }
+
+        for (let i = 0; i < crateCharIndexes.length; i++) {
+            const crateIndex = crateCharIndexes[i];
+            const stackIndex = i + 1;
+            const currentCrates = acc.get(stackIndex);
+
+            const crate = line[crateIndex];
+            const c = isEmpty(crate) ? [] : [crate];
+    
+            if (!currentCrates) {
+                acc.set(stackIndex, c);
+            } else {
+                acc.set(stackIndex, [...c, ...currentCrates]);
+            }
+    
+        }
+        return acc;
+    }, new Map());
+    
+    return mappedCrates;
+}
+
+export const day5 = async () => {
+    const input = splitByEmptyLine(await loadFile('day5.txt'));
+    const crates = input.slice(0, input.indexOf(''));
+    const instructions = input.slice(input.indexOf('') + 1);
+
+    const numbers = crates[crates.length - 1];
+    const crateCharIndexes: number[] = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        const char = numbers[i];
+        if (!isNaN(parseInt(char))) {
+            crateCharIndexes.push(i);
+        }
+    }
+
+    // prep program object
+    const program: Program = instructions.reduce((acc, line) => {
+        const v = line.split(' ')
+            .filter(v => !isNaN(parseInt(v)))
+            .map(c => parseInt(c));
+
+        acc.push({
+            q: v[0],
+            o: v[1],
+            d: v[2]
+        })
+        return acc;
+    }, [] as Program)
+
+    const mappedCrates = getMappedCrates(crates, crateCharIndexes);
+
+    return {
+        part1: exe(mappedCrates, program),
+        part2: exe(mappedCrates, program, false)
+    }
+}
