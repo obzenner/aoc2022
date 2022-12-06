abdf2901f35df217cb82042f5a55c324b29f7520
obzenner <dmitry.lambrianov@schibsted.com>
2022-12-06 18:50:56 +0100

    rmv console log

--- src/solutions/day6/index.ts
+++ src/solutions/day6/index.ts
@@ -23,7 +23,6 @@ const processDataScream = (input: string, limit = 4) => {
 export const day6 = async () => {
     const input = await loadFile('day6.txt');
 
-    console.log(processDataScream(input, 14))
     return {
         part1: processDataScream(input),
         part2: processDataScream(input, 14)
