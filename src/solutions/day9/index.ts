3cfe2bacb895d78ce180bb1a58f67fc868c86556
obzenner <dmitry.lambrianov@schibsted.com>
2022-12-11 06:45:34 +0100

    day9 part2

--- src/solutions/day9/index.ts
+++ src/solutions/day9/index.ts
@@ -18,53 +18,91 @@ export const day9 = async () => {
         'D': [0, -1]
     }
 
-    const calcTailPostion = (head: { x: number, y: number },
-        tail: { x: number, y: number },
-        dir: string) => {
+    const calcTailPostion = ({ head, tail, dir, isHead = true }: {
+        head: { x: number, y: number },
+        tail?: { x: number, y: number },
+        prevHead?: { x: number, y: number },
+        dir?: string,
+        isHead?: boolean
+    }) => {
         const [dx, dy] = directions[dir];
-        const prevHead = { ...head };
 
-        head.x += dx;
-        head.y += dy;
+        if (isHead) {
+            head.x += dx;
+            head.y += dy;
 
-        // Manhattan distance
-        const distance = Math.max(Math.abs(head.x - tail.x), Math.abs(head.y - tail.y));
-
-        if (distance > 1) {
-            tail = prevHead;
+            return { ...head };
         }
+
+        const diffX = Math.abs(head.x - tail.x);
+        const diffY = Math.abs(head.y - tail.y);
+
+        if (diffX < 2 && diffY < 2) {
+            return tail;
+        }
+
+        if (diffX > 1 && !diffY) {
+            tail.x += head.x - tail.x > 0 ? 1 : -1;
+          } else if (diffY > 1 && !diffX) {
+            tail.y += head.y - tail.y > 0 ? 1 : -1;
+          } else {
+            tail.x += head.x - tail.x > 0 ? 1 : -1;
+            tail.y += head.y - tail.y > 0 ? 1 : -1;
+          }
 
         return tail;
     }
 
-    const initPath = {
-        head: { x: 0, y: 0 },
-        tail: { x: 0, y: 0 },
-        memo: new Map(),
-        tailMemo: new Map(),
-    }
-    initPath.memo.set('x:0;y:0', 1);
-    initPath.tailMemo.set('x:0;y:0', 1);
+    const runWithManyTails = (input: { dir: string, dist: number }[], numberOfTails = 2) => {
+        const tails: {
+            position: { x: number, y: number },
+            memo: Set<string>
+        }[] = [];
 
-    const path = input.reduce((acc, step) => {
-        const { dir, dist } = step;
-
-        for (let i = 0; i < dist; i++) {
-            acc.tail = calcTailPostion(acc.head, acc.tail, dir);
-            const tailKey = `x:${acc.tail.x};y:${acc.tail.y}`;
-            const tailExists = acc.tailMemo.get(tailKey);
-            acc.tailMemo.set(tailKey, tailExists ? tailExists + 1 : 1);
-
-            const key = `x:${acc.head.x};y:${acc.head.y}`;
-            const exists = acc.memo.get(key);
-            acc.memo.set(key, exists ? exists + 1 : 1);
+        for (let i = 0; i < numberOfTails; i++) {
+            tails.push({
+                position: { x: 0, y: 0 },
+                memo: new Set(),
+            })
         }
 
-        return acc;
-    }, initPath);
+        tails.forEach(t => {
+            t.memo.add('x:0;y:0');
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            const step = input[i];
+            const { dir, dist } = step;
+
+            let currentHead;
+            for (let j = 0; j < dist; j++) {
+                for (let k = 0; k < tails.length; k++) {
+                    const tail = tails[k];
+                    const isHead = k === 0;
+
+                    if (isHead) {
+                        tail.position = calcTailPostion({ head: tail.position, dir });
+                    } else {
+                        currentHead = tails[k - 1];
+                        tail.position = calcTailPostion({
+                            head: currentHead.position,
+                            tail: tail.position,
+                            dir,
+                            isHead: false
+                        });
+                    }
+
+                    const tailKey = `x:${tail.position.x};y:${tail.position.y}`;
+                    tail.memo.add(tailKey);
+                }
+            }
+        }
+
+        return tails;
+    }
 
     return {
-        part1: path.tailMemo.size,
-        part2: 2
+        part1: runWithManyTails(input, 2)[1].memo.size,
+        part2: runWithManyTails(input, 10)[9].memo.size
     }
 }
