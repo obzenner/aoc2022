54dca828b09f350ab934adab61947f674a4cf6b3
obzenner <dmitry.lambrianov@schibsted.com>
2022-12-15 21:07:01 +0100

    day12

--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,23 @@ import { day8 } from './solutions/day8';
 import { day9 } from './solutions/day9';
 import { day10 } from './solutions/day10';
 import { day11 } from './solutions/day11';
+import { day12 } from './solutions/day12';
 
 
 (async () => {
     const res = {
-        day1: await day1(),
-        day2: await day2(),
-        day3: await day3(),
-        day4: await day4(),
-        day5: await day5(),
-        day6: await day6(),
-        day7: await day7(),
-        day8: await day8(),
-        day9: await day9(),
-        day10: await day10(),
-        day11: await day11(),
+        // day1: await day1(),
+        // day2: await day2(),
+        // day3: await day3(),
+        // day4: await day4(),
+        // day5: await day5(),
+        // day6: await day6(),
+        // day7: await day7(),
+        // day8: await day8(),
+        // day9: await day9(),
+        // day10: await day10(),
+        // day11: await day11(),
+        day12: await day12(),
     }
 
     console.log(res);
