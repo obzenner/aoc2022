2d124fe18f69c03acb9776b682a8b9feea1f67db
obzenner <dmitry.lambrianov@schibsted.com>
2022-12-17 17:01:50 +0100

    day12 part1

--- src/solutions/day12/index.ts
+++ src/solutions/day12/index.ts
@@ -2,8 +2,8 @@ import loadFile from "../../utils/loadFile";
 import { splitByEmptyLine } from "../../utils/splitByEmptyLine";
 
 const weightsAlpha: Record<string, number> = {
-  S: 0,
-  E: 0,
+  S: 1,
+  E: 27,
   a: 1,
   b: 2,
   c: 3,
@@ -54,32 +54,31 @@ const buildGraph = (input: string[]) => {
 
       const currNodes = {};
 
-      const currWeight = weightsAlpha[currentEl];
       if (nh1) {
         const nh1Weight = weightsAlpha[nh1];
         graph[currKey] = Object.assign(currNodes, {
-          [nh1Key]: Math.abs(nh1Weight - currWeight),
+          [nh1Key]: Math.abs(nh1Weight),
         });
       }
 
       if (nh2) {
         const nh2Weight = weightsAlpha[nh2];
         graph[currKey] = Object.assign(currNodes, {
-          [nh2Key]: Math.abs(nh2Weight - currWeight),
+          [nh2Key]: Math.abs(nh2Weight),
         });
       }
 
       if (nv1) {
         const nv1Weight = weightsAlpha[nv1];
         graph[currKey] = Object.assign(currNodes, {
-          [nv1Key]: Math.abs(nv1Weight - currWeight),
+          [nv1Key]: Math.abs(nv1Weight),
         });
       }
 
       if (nv2) {
         const nv2Weight = weightsAlpha[nv2];
         graph[currKey] = Object.assign(currNodes, {
-          [nv2Key]: Math.abs(nv2Weight - currWeight),
+          [nv2Key]: Math.abs(nv2Weight),
         });
       }
     }
@@ -88,15 +87,59 @@ const buildGraph = (input: string[]) => {
   return graph;
 }
 
+const bfsNodes = (graph: Record<string, Record<string, number>>, startNode: string, endNode: string) => {
+  const nodes: Record<string, string> = {};
+  nodes[startNode] = null;
+  const visited: Set<string> = new Set();
+  const queue = [startNode];
+
+  while (queue.length > 0) {
+    const currNode = queue.shift();
+    const currNodeNeighbors = graph[currNode];
+    const currWeight = weightsAlpha[currNode[0]];
+    
+    const destinations = Object.keys(currNodeNeighbors)
+    .filter(k => currNodeNeighbors[k] <= currWeight + 1);
+    
+    for (const d of destinations) {
+      if (!visited.has(d)) {
+        visited.add(d);
+        // eslint-disable-next-line no-prototype-builtins
+        if (!nodes.hasOwnProperty(d)) {
+          nodes[d] = currNode;
+        }
+        queue.push(d);
+      }
+      
+      console.log(d, currNode);
+      if (d === endNode) {
+        return nodes;
+      }
+    }
+  }
+
+  return nodes;
+}
+
 export const day12 = async () => {
   const input = splitByEmptyLine(await loadFile('day12.txt'));
 
   const graph = buildGraph(input);
-  const findEndNode = Object.keys(graph).find(k => k.includes('E'));
-  const findStartNode = Object.keys(graph).find(k => k.includes('S'));
+  const startNode = Object.keys(graph).find(k => k.includes('S'));
+  const endNode = Object.keys(graph).find(k => k.includes('E'));
+
+  // console.log(graph)
+  const nodes = bfsNodes(graph, startNode, endNode);
+	const steps = [];
+	let node = endNode;
+
+	while (nodes[node]) {
+		steps.push(node);
+		node = nodes[node];
+	}
 
   return {
-    part1: 1,
+    part1: steps.length,
     part2: 2
   }
 }
